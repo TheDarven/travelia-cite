2b31adbb536db3b69e91608e5433e33a03989f95
juloas <juloass@gmail.com>
2021-07-02 15:50:01 +0200

    Deposit ItemStack verification change
    Deposit ItemStack verification change

--- src/main/java/fr/thedarven/traveliacite/listener/listeners/DeposePointListener.java
+++ src/main/java/fr/thedarven/traveliacite/listener/listeners/DeposePointListener.java
@@ -1,7 +1,7 @@
 package fr.thedarven.traveliacite.listener.listeners;
 
-import fr.thedarven.traveliacite.Cite;
-import fr.thedarven.traveliacite.player.model.PlayerCite;
+import java.util.Objects;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
@@ -13,8 +13,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Objects;
+import fr.thedarven.traveliacite.Cite;
+import fr.thedarven.traveliacite.player.model.PlayerCite;
 
 public class DeposePointListener implements Listener {
 
@@ -38,30 +40,38 @@ public class DeposePointListener implements Listener {
                 e.setCancelled(true);
 
                 PlayerInventory playerInventory = player.getInventory();
-                int totalPoints = 0;
-
-                for (int i = 0; i < playerInventory.getSize(); i++) {
-                    ItemStack item = playerInventory.getItem(i);
-                    if (Objects.isNull(item) || item.getType() != Material.HAY_BLOCK) {
-                        continue;
-                    }
-
-                    totalPoints += item.getAmount();
-                    playerInventory.setItem(i, null);
-                }
-
-                if (totalPoints > 0) {
-                    PlayerCite playerCite = this.main.getPlayerManager().getAndCreatePlayerCiteByPlayer(player);
-                    System.out.println("[Cite] " + player.getName() + " a déposé " + totalPoints + " points. (" + playerCite.getEmeralds() + "P)");
-                    playerCite.addEmeralds(totalPoints);
-                    player.sendMessage("§2" + totalPoints + "§a points ont été déposés sur votre compte.");
-                    Objects.requireNonNull(clickedPnjLocation.getWorld())
-                            .spawnParticle(Particle.VILLAGER_HAPPY, clickedPnjLocation.add(0, 2, 0),10, 0.5, 0.25, 0.5);
-                } else {
-                    player.sendMessage("§eVous n'avez aucun point on déposer sur vous.");
-                }
-            }
+                playerInventory.forEach(i -> tryConsumeStack(i, player, clickedPnjLocation));  
+            }   
         }
+    }
+    
+    public void tryConsumeStack(ItemStack stack, Player player, Location clickedPnjLocation) {
+    	
+    	if(!isStackValid(stack)) return;
+    	int amount = stack.getAmount();
+    	
+    	if (amount > 0) {
+            PlayerCite playerCite = this.main.getPlayerManager().getAndCreatePlayerCiteByPlayer(player);
+            System.out.println("[Cite] " + player.getName() + " a déposé " + amount + " points. (" + playerCite.getEmeralds() + "P)");
+            playerCite.addEmeralds(amount);
+            player.sendMessage("§2" + amount + "§a points ont été déposés sur votre compte.");
+            Objects.requireNonNull(clickedPnjLocation.getWorld()).spawnParticle(Particle.VILLAGER_HAPPY, clickedPnjLocation.add(0, 2, 0), 10, 0.5, 0.25, 0.5);
+        } else player.sendMessage("§eVous n'avez aucun point a déposer sur vous.");
+    	
+    	// Remove stack, but with more steps, just in case, to hopefully prevent unintentional duplication glitch
+    	player.getInventory().remove(stack);
+    	stack.setAmount(0);
+    	stack.setType(Material.AIR);
+    	
+    }
+    
+    public boolean isStackValid(ItemStack stack) {
+    	if(stack.getType() == Material.HAY_BLOCK) return false;
+    	if(stack.getAmount() <= 0) return false;
+    	ItemMeta meta = stack.getItemMeta();
+    	if(meta.getCustomModelData() != 789) return false;
+    	// ADD SOME CHECKS HERE if needed
+    	return true;
     }
 
 }
